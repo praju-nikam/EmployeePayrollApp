1fc303265735da3fed6ee9e8f99b468adf28a776
praju-nikam <sawanprajakta84@gmail.com>
2022-06-17 15:40:47 +0530

    Added: Perform Validation Employee Payroll Data setter methods

---
+++ Day-43-EmployeePayroll-App/JS/EmployeePayrollData.js
@@ -0,0 +1,56 @@
+// UC-9
+class EmployeePayrollData {
+
+
+    //getter and setter method
+    id() {
+        return this._id = id;
+    }
+    id(id) {
+        this._id = id;
+    }
+    name() {
+        return this._name;
+    }
+    set name(name) {
+
+        this._name = name;
+    }
+    profilePic() {
+        return this._profilePic;
+    }
+    profilePic(profilePic) {
+        this._profilePic = profilePic;
+    }
+    gender() {
+        return this._gender;
+    }
+    gender(gender) {
+        this._gender = gender;
+    }
+    get department() {
+        return this._deparment;
+    }
+    set department(department) {
+        this._deparment = department;
+    }
+    get salary() {
+        return this._salary;
+    }
+    set salary(salary) {
+        this._salary = salary;
+    }
+    get note() {
+        return this._note;
+    }
+    set note(note) {
+        this._note = note;
+    }
+    get startDate() {
+        return this._startDate;
+    }
+    set startDate(startDate) {
+
+        this._startDate = startDate;
+    }
+}
